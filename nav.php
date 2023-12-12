0022d3d906c4ff4b8c92fb9647f25cffe14c2f23
turkerdev <48413508+turkerdev@users.noreply.github.com>
2023-12-12 20:43:20 +0300

    make navbar dynamic to session, add logout
    Co-authored-by: SM4142 <SM4142@users.noreply.github.com>

--- nav.php
+++ nav.php
@@ -1,6 +1,18 @@
+<?php 
+  require_once "session.php";
+  require_once "db.php";
+?>
+
 <div class="nav">
   <a href="anasayfa.php">Ana sayfa</a>
   <a href="ara.php">Ara</a>
-  <a href="giris.php">Giris</a>
-  <a href="kayitol.php">Kayıt</a>
+  <?php if(!empty($_SESSION['id'])) { ?>
+    <form method="post" action="cikis.php">
+      <button name="cikis">Cikis yap</button>
+    </form>
+    <p>Hoşgeldin, <?php echo $db->query("SELECT * FROM kullanicilar WHERE id = " . $_SESSION['id'])->fetch()['kullanici_adi']?></p>
+  <?php } else { ?>
+    <a href="giris.php">Giris</a>
+    <a href="kayitol.php">Kayıt</a>
+  <?php } ?>
 </div>
